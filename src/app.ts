c35f0e67b0a99482c6374bbae5d961b296509e8c
sharlq <shehabakhader@gmail.com>
2023-07-02 14:33:51 +0300

    removing secrits

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
+const frontEndPath = path.resolve(__dirname, '..', 'client', 'dist');
 
 app.use(bodyParser.json());
+// Serve static files from the client/dist directory
+app.use(express.static(frontEndPath));
 
-app.use('/', (req, res) => {
-  res.send('Hello World');
+app.get('*', (req, res) => {
+  console.log(frontEndPath);
+  res.sendFile(path.join(frontEndPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
